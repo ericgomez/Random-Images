423dd84269549ef8de871b137c4caab2ce9573f6
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-23 10:21:46 -0500

    Arquitectura Componentes Lifecycle Observe

--- app/src/main/java/com/esgomez/randomimages/MainActivity.kt
+++ app/src/main/java/com/esgomez/randomimages/MainActivity.kt
@@ -2,7 +2,10 @@ package com.esgomez.randomimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.squareup.picasso.Picasso
@@ -13,12 +16,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var imagePhoto: ImageView = findViewById(R.id.imagePhoto)
+        var imagePhoto2: ImageView = findViewById(R.id.imagePhoto2)
+        var btnGetUrlImage: Button = findViewById(R.id.btnGetUrlImage)
 
         //var model: MainActivityViewModel = MainActivityViewModel()
         //Damos de alta la clase como Provider esto permitira que los datos persistan al girar el dispositivo
         var model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        //insertamos la Photo
-        Picasso.get().load(model.callUrlImage()).into(imagePhoto)
+        var urlImage: MutableLiveData<String?>? = model.callUrlImage()
+
+        urlImage?.observe(this, Observer {
+            print("Se ejecuta si la url sufre un cambio")
+            //insertamos la Photo que viene en el string it
+            Picasso.get().load(it).into(imagePhoto)
+            Picasso.get().load(it).into(imagePhoto2)
+        })
+
+        btnGetUrlImage.setOnClickListener {//Ese cambio ocurre cuando doy click en el boton
+            model.randomNumbresUrl()
+        }
     }
 }
--- app/src/main/java/com/esgomez/randomimages/MainActivityViewModel.kt
+++ app/src/main/java/com/esgomez/randomimages/MainActivityViewModel.kt
@@ -1,20 +1,22 @@
 package com.esgomez.randomimages
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
 
-    private var urlImage: String? = null
+    private var urlImage: MutableLiveData<String?>? = null
 
-    fun callUrlImage(): String? {
+    fun callUrlImage(): MutableLiveData<String?>? {
         if (urlImage == null) {
-            urlImage = randomNumbresUrl()
+            urlImage = MutableLiveData<String?>()
+            randomNumbresUrl()
         }
 
         return urlImage
     }
     //
-    fun randomNumbresUrl(): String {
-        return "https://picsum.photos/${(3..5).random()}00/${(3..5).random()}00"
+    fun randomNumbresUrl() {
+        urlImage?.value = "https://picsum.photos/${(3..5).random()}00/${(3..5).random()}00"
     }
 }
